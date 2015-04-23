5b3ff162e6b092603dc3a74ac65c162022480339
Artem <mrlaiko@yandex.ua>
2015-04-23 23:24:53 +0300

    Deploy v0.1
    First version of library

---
+++ lib/config.php
@@ -0,0 +1,14 @@
+<?php
+
+    define("userTable", "dle_users");                  // Таблица с пользователями вашего движка
+    define("userBTable", "dle_users");                 // Таблица в которой содержатся значения баланса пользователя
+    define("userNColumn", "username");                 // Таблица с именем пользователя
+    define("userBColumn", "money");                    // Колонка в деньгами пользователя
+    define("permissionsTable", "permissions_entity");  // Таблица PEX с пользователями
+    define("permissions", "permissions");              // Таблица PEX с группами
+
+    define("DBHOST", "localhost"); //  Хост базы данных
+    define("DBUSER", "root");      //  Пользователь БД
+    define("DBPASS", "");          //  Пароль БД
+    define("DBNAME", "mpcl");        //  Имя БД
+?>
